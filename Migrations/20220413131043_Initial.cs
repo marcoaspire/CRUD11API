d98e56f8fea46e2116286cbbf7d5e8c7d84f9426
marcoaspire <marco.antonio@aspiresys.com>
2022-04-14 14:36:55 -0500

    upload api paymethd

---
+++ Migrations/20220413131043_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crud11API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails2",
+                columns: table => new
+                {
+                    PaymentDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    CardNumber = table.Column<string>(type: "varchar(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "varchar(5)", nullable: false),
+                    SecurityCode = table.Column<string>(type: "varchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails2", x => x.PaymentDetailId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails2");
+        }
+    }
+}
---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud11API.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        { }
+
+        public DbSet<PaymentDetail2> PaymentDetails { get; set; }
+    }
+}
